ba4b97c5bbdc90b5649ad1c06ac6e87306ec1205
Walid Baharwal <walidbaharwal5400@gmail.com>
2024-05-17 18:00:34 +0500

    feat(api): added getvideo and tweet comments controller functions (include pagination ) and there api routes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -2,16 +2,17 @@ import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Comment } from "../models/comment.model.js";
+import mongoose from "mongoose";
 
 const addVideoComment = asyncHandler(async (req, res) => {
-    const { content, videoId, userId } = req.body;
+    const { content, videoId } = req.body;
     if (!content || !videoId) {
         throw new apiError(400, "Fiels can't be empty");
     }
     const comment = await Comment.create({
         content,
         video: videoId,
-        owner: userId,
+        owner: req.user._id,
     });
 
     if (!comment) {
@@ -20,14 +21,14 @@ const addVideoComment = asyncHandler(async (req, res) => {
     res.status(200).json(new apiResponse(200, comment, "Comment added successfully"));
 });
 const addTweetComment = asyncHandler(async (req, res) => {
-    const { content, tweetId, userId } = req.body;
+    const { content, tweetId } = req.body;
     if (!content || !tweetId) {
         throw new apiError(400, "Fiels can't be empty");
     }
     const tweet = await Comment.create({
         content,
         tweet: tweetId,
-        owner: userId,
+        owner: req.user._id,
     });
 
     if (!tweet) {
@@ -46,4 +47,91 @@ const deleteComment = asyncHandler(async (req, res) => {
     res.status(200).json(new apiResponse(200, {}, "Comment deleted successfully"));
 });
 
-export { addVideoComment, addTweetComment, deleteComment };
+const getVideoComments = asyncHandler(async (req, res) => {
+    const options = {
+        page: req.query.page || 1,
+        limit: req.query.limit || 20,
+        sort: req.query.sort ? { createdAt: parseInt(req.query.sort) } : { createdAt: -1 },
+    };
+    const { videoId } = req.body;
+    const pipeline = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner",
+                },
+            },
+        },
+    ]);
+    const comments = await Comment.aggregatePaginate(pipeline, options);
+    if (!comments) {
+        throw new apiError(500, "Error fetching Comments");
+    }
+    res.status(200).json(new apiResponse(200, comments, "Video Comments fetched successfully"));
+});
+const getTweetComments = asyncHandler(async (req, res) => {
+    const options = {
+        page: req.query.page || 1,
+        limit: req.query.limit || 20,
+        sort: req.query.sort ? { createdAt: parseInt(req.query.sort) } : { createdAt: -1 },
+    };
+    const { tweetId } = req.body;
+    const pipeline = Comment.aggregate([
+        {
+            $match: {
+                tweet: new mongoose.Types.ObjectId(tweetId),
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            "avatar.url": 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner",
+                },
+            },
+        },
+    ]);
+    const comments = await Comment.aggregatePaginate(pipeline, options);
+    if (!comments) {
+        throw new apiError(500, "Error fetching Comments");
+    }
+    res.status(200).json(new apiResponse(200, comments, "Tweets Comments fetched successfully"));
+});
+
+export { addVideoComment, addTweetComment, deleteComment, getVideoComments, getTweetComments };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addVideoComment, addTweetComment, deleteComment } from "../controllers/comment.controller.js";
+import {
+    addVideoComment,
+    addTweetComment,
+    deleteComment,
+    getVideoComments,
+    getTweetComments,
+} from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.route("/vc").post(verifyJWT, addVideoComment);
 router.route("/tc").post(verifyJWT, addTweetComment);
 router.route("/delete/:_id").post(verifyJWT, deleteComment);
+router.route("/get-video-comments").get(getVideoComments);
+router.route("/get-tweet-comments").get(getTweetComments);
 
 export default router;
